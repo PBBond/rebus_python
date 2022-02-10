32c75a69807b40fa1de39a6b0dafc6a5c8e4e409
PBBond <97637272+PBBond@users.noreply.github.com>
2022-02-09 18:32:37 -0600

    basic flask

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #test file to make sure git is working
 #test
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
